1ad7eb189380f125d6d79adac3e518b86313c7c2
HAMIDREZA <soltanshahhamidreza@gmail.com>
2025-12-10 14:18:29 +0000

    Extract VPN files

--- iranian_pro_vpn/android/app/src/main/kotlin/com/iranianprovpn/app/Application.kt
+++
@@ -1,7 +0,0 @@
-package com.iranianprovpn.app
-
-import android.app.Application
-
-class Application : Application() {
-    // برای Flutter جدید نیازی به کد خاصی نیست
-}
--- iranian_pro_vpn/android/build.gradle.kts
+++
@@ -1,3 +0,0 @@
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory.get().asFile)  // fix: deprecated buildDir → layout.buildDirectory
-}
